c97185fd5fb4eaed59ec8abf1446d296a2e65c13
daicham <daicham@gmail.com>
2011-04-04 22:58:50 +0900

    ti2-1-1 タイムライン表示のためのテーブルビューを作成

--- Resources/app.js
+++ Resources/app.js
@@ -4,7 +4,6 @@ Titanium.UI.setBackgroundColor('#000');
 // create tab group
 var tabGroup = Titanium.UI.createTabGroup();
 
-
 //
 // create base UI tab and root window
 //
@@ -13,52 +12,18 @@ var win1 = Titanium.UI.createWindow({
     backgroundColor:'#fff'
 });
 var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
     window:win1
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+var tableView = Ti.UI.createTableView({
+    data:[]
 });
-
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
+win1.add(tableView);
+win1.hideTabBar();
 //
 //  add tabs
 //
 tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
 
 // open tab group
 tabGroup.open();
