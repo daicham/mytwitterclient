2e809c54fa9b8a22003b8645c3fd6522135116f6
daicham <daicham@gmail.com>
2011-04-05 00:47:54 +0900

    ti 3-3-1 アプリ本体(Window)とTableViewのソースコードを分割した。

--- Resources/app.js
+++ Resources/app.js
@@ -8,6 +8,7 @@ var tabGroup = Titanium.UI.createTabGroup();
 // create base UI tab and root window
 //
 var win1 = Titanium.UI.createWindow({  
+    url: 'table_view.js',
     title:'Tab 1',
     backgroundColor:'#fff'
 });
@@ -15,88 +16,6 @@ var tab1 = Titanium.UI.createTab({
     window:win1
 });
 
-//画面表示時はまだデータとれていないので(非同期だから)最初は空にしておく
-var data = [];
-var tableView = Ti.UI.createTableView({
-    data:data
-});
-
-function updateTimeline (timeline) {
-    var currentData = [];
-    for (var i=0; i<timeline.length;i++) {
-        var tweet = timeline[i];
-
-        var row = Ti.UI.createTableViewRow(
-            {
-                height: 'auto',
-                layout: 'vertical'
-            }
-        );
-
-        var imageView = Ti.UI.createImageView(
-            {
-                image: tweet.user.profile_image_url,
-                width: 48,
-                height: 48,
-                top: 5,
-                left: 5
-            }
-        );
-        row.add(imageView);
-
-        var nameLabel = Ti.UI.createLabel(
-            {
-                width: 120,
-                height: 'auto',
-                left: 58,
-                top: -48,
-                fontSize: 6,
-                fontWeight: 'bold',
-                color: '#2b4771'
-            }
-        );
-        nameLabel.text = tweet.user.screen_name;
-        row.add(nameLabel);
-
-        var commentLabel = Ti.UI.createLabel(
-            {
-                width: 257,
-                left: 58,
-                top: 1,
-                height: 'auto',
-                fontSize: 8
-            }
-        );
-        commentLabel.text = tweet.text;
-        row.add(commentLabel);
-
-        var dateLabel = Ti.UI.createLabel(
-            {
-                width: 200,
-                height: 'auto',
-                left: 58,
-                bottom: 8,
-                fontSize: 6
-            }
-        );
-        dateLabel.text = tweet.created_at;
-        row.add(dateLabel);
-        currentData.push(row);
-    }
-    tableView.setData(currentData);
-}
-
-var xhr = Ti.Network.createHTTPClient(); //Ti.Network.HTTPClientは非同期で動く
-var user = 'daicham';
-var url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + user;
-xhr.open('GET', url);
-xhr.onload = function() {
-    var timeline = JSON.parse(this.responseText);
-    updateTimeline(timeline);
-};
-xhr.send();
-
-win1.add(tableView);
 win1.hideTabBar();
 //
 //  add tabs
